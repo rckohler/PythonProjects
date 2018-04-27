3d683a1f54f7057937c54e07d71b3b8b72fcd50d
rckohler <tfbabcl@gmail.com>
2018-04-27 14:00:05 -0700

    Elijah combat finished

--- classExample.py
+++ classExample.py
@@ -30,33 +30,36 @@ def getInt(prompt,max,min = 1):
                 print("out of range")
         except:
             print("invalid character... whole numbers only.")
-a = getInt("pick a number between 1 and 10" , 10)
+#a = getInt("pick a number the same or less then the number of points you have left")
 class Guy:
-
     def __init__(self, player = False):
         if not player:
             names = ["Abe","Bob","Carl","Doug","Earl","Fred","Greg","Hal","Igor","Jayce","Kayel","Lambda","Mohawk","Ned","Owpen","Peter","Quin","Ryen","Seld","Tae","Ulysses","Victor","Wan","Xan","Yan","Zed"]
             self.name = random.choice(names) + " " + random.choice(names)+ "son"
-            self.strength = random.randint(1,6)
-            self.speed = random.randint(1,6)
-            self.tough = random.randint(1,6)
+            self.strength = random.randint(1,4)
+            self.speed = random.randint(1,4)
+            self.tough = random.randint(1,4)
         else:
             done = False
             pointsRemaining = 7
 
             while not done:
                 self.name = input ("what is your name")
-                self.tough = int(input ("U have 7 points,"
-                                        "use them to upgrade your attributes. How many will u spend on strength"))
-                self.speed = input ("What will u spend on speed, "
-                                    "if u have less then zero at then end u will have to restart.")
-                self.strength = input("What will u use on strength")
+                self.tough = getInt("U have 10 points,"
+                                        "use them to upgrade your attributes. How many will u spend on tough",6)
+                pointsRemaining-=self.tough-1
+                self.speed = getInt ("What will u spend on speed, "
+                                    "if u have less then zero at then end u will have to restart.",pointsRemaining+1)
+                pointsRemaining-=self.speed-1
+                self.strength = getInt("What will u use on strength",pointsRemaining+1)
+                pointsRemaining-=self.strength-1
+                print(pointsRemaining)
                 if pointsRemaining == 0:
                     print("good job")
                     done = True
                 else:
                     print("cheater")
-            self.maxhp = self.tough
+        self.maxhp = self.tough
 
     def introduce(self):
         print(self.name + "'s strength = " + str(self.strength))
@@ -66,7 +69,7 @@ class Guy:
         x = victim.strength
         a = self.strength
         attackRoll = random.randint(1,100)+ victim.speed*2
-
+        print(self.name + " tough = " + str(self.tough))
         if attackRoll <= 10:
             print(self.name + " destroyed " + victim.name+".")
             victim.tough -= 2*self.strength
@@ -75,10 +78,10 @@ class Guy:
             victim.tough -=self.strength
         elif attackRoll<=50:
             print(self.name + " nicked " + victim.name + ".")
-            self.tough -= (1/2)
+            victim.tough -= (1/2)
         else:
             print(self.name + " fails at hitting " + victim.name + ".")
-player = Guy(True)
+
 def runSimulatedCombat():
     guys = []
     for i in range(1):
@@ -97,3 +100,18 @@ def runSimulatedCombat():
         guy.introduce()
 
 
+def killCharacter():
+    player = Guy(True)
+    kills = 0
+    while player.tough > 0:
+        enemy = Guy()
+        while enemy.tough > 0 and player.tough > 0:
+            player.attack(enemy)
+            enemy.attack(player)
+        if enemy.tough <=0:
+            kills +=1
+            if player.tough < player.maxhp:
+                player.tough += 0.0*(player.maxhp-player.tough)
+    print("you have died but you killed " + str(kills) + " enemies.")
+
+killCharacter()
